a47fc0eab7a30cc86ec7bd97cd777f7b1e05f617
jimmyfargo <zblizzard@live.com>
2017-05-10 07:49:48 -0500

    updating the zone controller to handle comma separated zip codes

--- controllers/ZoneController.js
+++ controllers/ZoneController.js
@@ -23,6 +23,16 @@ module.exports = {
     },
 
     create: (params, callback) => {
+        var zips = params['zipCodes']
+        var zipCodes = zips.split(',')
+        var newZips = []
+        zipCodes.forEach((zip) => {
+            newZips.push(zip.trim())
+        })
+
+        params['zipCodes'] = newZips.filter((zip) => {
+            return zip.length >= 5
+        })
         Zone.create(params, (err, zone) => {
             if (err) {
                 callback(err, null)
@@ -33,8 +43,15 @@ module.exports = {
         })
     },
 
-    update: () => {
+    update: (id, params, callback) => {
+        Zone.findByIdAndUpdate(id, params, { new: true }, (err, zone) => {
+            if (err) {
+                callback(err, null)
+                return
+            }
 
+            callback(null, zone)
+        })
     },
 
     destroy: () => {
